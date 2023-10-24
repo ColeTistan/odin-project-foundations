876a80b6aacb0290737e826f30c6bbecd31e40f2
coletistan <coletistan@gmail.com>
2023-10-24 15:03:18 -0400

    Built UI with CSS for UX interaction with game

--- odin-rock-paper-scissors/app.js
+++ odin-rock-paper-scissors/app.js
@@ -1,73 +1,73 @@
-// initialize fields and DOM objects.
-let choices = ["rock", "paper", "scissors"];
-const startBtn = document.querySelector("button");
+// // initialize fields and DOM objects.
+// let choices = ["rock", "paper", "scissors"];
+// const startBtn = document.querySelector("button");
 
-const getComputerChoice = function () {
-    const randChoice = Math.random() * choices.length;
-    return choices[Math.floor(randChoice)].toLowerCase();
-}
+// const getComputerChoice = function () {
+//     const randChoice = Math.random() * choices.length;
+//     return choices[Math.floor(randChoice)].toLowerCase();
+// }
 
-const validateplayerChoice = function (playerChoice) {
-    return choices.includes(playerChoice);
-}
+// const validateplayerChoice = function (playerChoice) {
+//     return choices.includes(playerChoice);
+// }
 
-// get user input and validate
-const getUserInput = function () {
-    let playerChoice = prompt("Enter a choice: Rock, Paper, Scissors");
-    while (playerChoice == null) {
-        playerChoice = prompt("Enter a choice: Rock, Paper, Scissors");
-    }
-    while (validateplayerChoice(playerChoice.toLowerCase()) != true) {
-        playerChoice = prompt("Enter a valid choice: Rock, Paper, Scissors");
-    }
-    return playerChoice.toLowerCase();
-}
+// // get user input and validate
+// const getUserInput = function () {
+//     let playerChoice = prompt("Enter a choice: Rock, Paper, Scissors");
+//     while (playerChoice == null) {
+//         playerChoice = prompt("Enter a choice: Rock, Paper, Scissors");
+//     }
+//     while (validateplayerChoice(playerChoice.toLowerCase()) != true) {
+//         playerChoice = prompt("Enter a valid choice: Rock, Paper, Scissors");
+//     }
+//     return playerChoice.toLowerCase();
+// }
 
-// this function will check player and computer choice to determine winner.
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toUpperCase() == computerSelection.toUpperCase()) {
-        return "tie";
-    }
-    if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "PAPER" ||
-        playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "ROCK" ||
-        playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "SCISSORS") {
-        return "player";
-    }
-    if (playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "SCISSORS" ||
-        playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "PAPER" ||
-        playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "ROCK") {
-        return 'computer';
-    }
-}
+// // this function will check player and computer choice to determine winner.
+// function playRound(playerSelection, computerSelection) {
+//     if (playerSelection.toUpperCase() == computerSelection.toUpperCase()) {
+//         return "tie";
+//     }
+//     if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "PAPER" ||
+//         playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "ROCK" ||
+//         playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "SCISSORS") {
+//         return "player";
+//     }
+//     if (playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "SCISSORS" ||
+//         playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "PAPER" ||
+//         playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "ROCK") {
+//         return 'computer';
+//     }
+// }
 
 
-const game = function () {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 1; i <= 5; i++) {
-        playerChoice = getUserInput();
-        computerChoice = getComputerChoice();
-        console.log(playerChoice, computerChoice);
-        roundWinner = playRound(playerChoice, computerChoice);
-        console.log(`Player choice: ${playerChoice}, Computer choice: ${computerChoice}`);
-        if (roundWinner == 'tie') {
-            console.log("It's a tie!");
-        } else if (roundWinner == 'computer') {
-            computerScore++;
-            console.log("Computer won!");
-        } else {
-            playerScore++;
-            console.log("Player won!");
-        }
-    }
-    const finalScoreMessage = `Final Score - Player: ${playerScore} Computer: ${computerScore}`;
-    if (playerScore == computerScore) {
-        console.log(`${finalScoreMessage}. It's a tie!`);
-    } else if (playerScore < computerScore) {
-        console.log(`${finalScoreMessage}. Computer won!`);
-    } else {
-        console.log(`${finalScoreMessage}. Player won!`);
-    }
-}
+// const game = function () {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     for (let i = 1; i <= 5; i++) {
+//         playerChoice = getUserInput();
+//         computerChoice = getComputerChoice();
+//         console.log(playerChoice, computerChoice);
+//         roundWinner = playRound(playerChoice, computerChoice);
+//         console.log(`Player choice: ${playerChoice}, Computer choice: ${computerChoice}`);
+//         if (roundWinner == 'tie') {
+//             console.log("It's a tie!");
+//         } else if (roundWinner == 'computer') {
+//             computerScore++;
+//             console.log("Computer won!");
+//         } else {
+//             playerScore++;
+//             console.log("Player won!");
+//         }
+//     }
+//     const finalScoreMessage = `Final Score - Player: ${playerScore} Computer: ${computerScore}`;
+//     if (playerScore == computerScore) {
+//         console.log(`${finalScoreMessage}. It's a tie!`);
+//     } else if (playerScore < computerScore) {
+//         console.log(`${finalScoreMessage}. Computer won!`);
+//     } else {
+//         console.log(`${finalScoreMessage}. Player won!`);
+//     }
+// }
 
-startBtn.addEventListener("click", game());
+// startBtn.addEventListener("click", game());
