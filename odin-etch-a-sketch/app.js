7f424ffc0848fec93a4b411f1096609cb949eb03
coletistan <coletistan@gmail.com>
2023-11-06 14:14:28 -0500

    added draw and grid size functionality to allow change to size and color of divs.

--- odin-etch-a-sketch/app.js
+++ odin-etch-a-sketch/app.js
@@ -1,15 +1,25 @@
+// Get and declare each button
+const btns = document.getElementsByTagName('button');
+const eraserBtn = btns[0];
+const randomBtn = btns[1];
+const resetBtn = btns[2];
+const smallGridBtn = btns[3];
+const mediumGridBtn = btns[4];
+const largeGridBtn = btns[5];
+
+console.log(btns);
+
 // Generates squares and inserts into canvas
 const createSquares = function(size) {
     let canvas = document.querySelector('.canvas');
     let squareDivs = canvas.querySelectorAll('div');
     let gridSize = size * size;
 
-    // Remove all square divs before prepopulating canvas.
+    // Remove all square divs before prepopulating canvas
     squareDivs.forEach(function(div) {
-        squareDivs.remove(div);
+        div.remove();
     });
 
-    // canvas.style.width = `${}px`;
     canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
@@ -19,7 +29,31 @@ const createSquares = function(size) {
         squareDiv.classList.add('square');
         canvas.insertAdjacentElement('beforeend', squareDiv);
     }
-    
+}
+
+// Enable draw mode to allow user to fill squares
+const draw = function() {
+    let canvas = document.querySelector('.canvas');
+    let squareDivs = canvas.querySelectorAll('div');
+    squareDivs.forEach(function(div) {
+        div.addEventListener('mouseover', function() {
+            div.style.background = 'var(--dark-color)';
+        });
+    });
 }
 
 createSquares(16);
+draw();
+
+smallGridBtn.addEventListener('click', function() {
+    createSquares(16);
+    draw();
+});
+mediumGridBtn.addEventListener('click', function() {
+    createSquares(32);
+    draw();
+});
+largeGridBtn.addEventListener('click', function() {
+    createSquares(64);
+    draw();
+});
