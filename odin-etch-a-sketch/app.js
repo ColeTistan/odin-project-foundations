04d19f0b43eeb0271588182cab4a21588aa81820
coletistan <coletistan@gmail.com>
2023-11-07 13:40:10 -0500

    added user input functionality to allow user to enter grid size

--- odin-etch-a-sketch/app.js
+++ odin-etch-a-sketch/app.js
@@ -3,19 +3,41 @@ const btns = document.getElementsByTagName('button');
 const eraserBtn = btns[0];
 const randomBtn = btns[1];
 const clearBtn = btns[2];
-const smallGridBtn = btns[3];
-const mediumGridBtn = btns[4];
-const largeGridBtn = btns[5];
+const submitBtn = btns[3];
 
-let currentColor = '#222222';
+let currentColor = 'var(--black)';
 
-const draw = function(squareDiv) {
-    console.log(currentColor);
-    squareDiv.style.background = currentColor;
+// clears all colored divs on canvas back to original color
+const clearCanvas = function() {
+    let canvas = document.querySelector('.canvas');
+    let squareDivs = canvas.querySelectorAll('div');
+    squareDivs.forEach(function(div) {
+        div.style.background = 'var(--light)';
+    })
+    currentColor = 'var(--black)';
+}
+
+// retrieves current color value set to determine
+// what button functionality will be triggered.
+const updateColor = function(colorSelected) {
+    currentColor = colorSelected;
+}
+
+// retrieves user input to generate some number of grids on canvas
+const getUserInput = function() {
+    let gridInput = document.getElementById('grid-input');
+    if (gridInput.value < 0 || gridInput.value > 100) {
+        alert("Number entered is smaller than 0 or greater than 100. Please enter a different number.");
+        gridInput.value = 16;
+        return gridInput.value;
+    } else {
+        return gridInput.value;
+    }
 }
 
 // Generates squares and inserts into canvas
-const createSquares = function(size) {
+const createSquares = function() {
+    let size = getUserInput();
     let canvas = document.querySelector('.canvas');
     let squareDivs = canvas.querySelectorAll('div');
     let gridSize = size * size;
@@ -31,49 +53,30 @@ const createSquares = function(size) {
     // populate grid with given size
     for (let i = 0; i < gridSize; i++) {
         let squareDiv = document.createElement('div');
-        currentColor = '#222222';
+        currentColor = 'var(--black)';
         squareDiv.addEventListener('mouseover', function() {
             switch (currentColor) {
                 case 'eraser':
-                    squareDiv.style.background = 'var(--white)';
+                    squareDiv.style.background = 'var(--light)';
                     break;
                 case 'random':
                     squareDiv.style.background = `hsl(${Math.floor(Math.random() * 255)}, 100%, 50%)`;
                     break;
                 default:
-                    squareDiv.style.background = '#222222';
+                    squareDiv.style.background = 'var(--black)';
                     break;
             }
         });
         squareDiv.style.border = 'solid 1px #d3d3d3';
-        squareDiv.style.background = '#ffffff';
+        squareDiv.style.background = 'var(--light)';
         canvas.insertAdjacentElement('beforeend', squareDiv);
     }
 }
 
-const clearCanvas = function() {
-    let canvas = document.querySelector('.canvas');
-    let squareDivs = canvas.querySelectorAll('div');
-    squareDivs.forEach(function(div) {
-        div.style.background = 'var(--white)';
-    })
-    currentColor = '#222222';
-}
+// button events to trigger UI functionality
 
-const updateColor = function(colorSelected) {
-    currentColor = colorSelected;
-}
-
-smallGridBtn.addEventListener('click', function() {
-    createSquares(16);
-});
-
-mediumGridBtn.addEventListener('click', function() {
-    createSquares(32);
-});
-
-largeGridBtn.addEventListener('click', function() {
-    createSquares(64)
+submitBtn.addEventListener('click', function() {
+    createSquares();
 });
 
 clearBtn.addEventListener('click', clearCanvas());
