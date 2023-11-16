7e5052cab33db6751fa4a6b9ee1dcc4d43b561f5
coletistan <coletistan@gmail.com>
2023-11-15 20:22:08 -0500

    Updated CSS styling and added logic to check for operator in first element in calc value list.

--- calculator/app.js
+++ calculator/app.js
@@ -4,7 +4,12 @@ let secondNum = 0;
 let operator = '';
 
 const buttons = document.querySelectorAll('button');
-const displayScreen = document.querySelector('.display-screen');
+const mainDisplayScreen = document.querySelector('.display-screen');
+const subdisplayScreens = mainDisplayScreen.querySelectorAll('p');
+const autoClearButton = document.getElementById('auto-clear');
+
+let subdisplayText = subdisplayScreens[0];
+let subdisplayResults = subdisplayScreens[1];
 
 const add = function(firstNum, secondNum) {
     return parseFloat(firstNum) + parseFloat(secondNum);
@@ -15,12 +20,20 @@ const subtract = function(firstNum, secondNum) {
 }
 
 const multiply = function(firstNum, secondNum) {
-    return parseFloat(firstNum) * parseFloat(secondNum);
+    return Math.round(parseFloat(firstNum) * parseFloat(secondNum) * 100) / 100;
 }
 
 const divide = function(firstNum, secondNum) {
-    let result = firstNum / secondNum;
-    return result == 'Infinity' ? 'Undefined' : parseFloat(firstNum) / parseFloat(secondNum);
+    let result = Math.round(parseInt(firstNum) / parseFloat(secondNum) * 100) / 100;
+    return result == 'Infinity' ? 'Undefined' : Math.round(parseFloat(firstNum) / parseFloat(secondNum) * 100) / 100;
+}
+
+const modulus = function(firstNum, secondNum) {
+    return Math.round(parseFloat(firstNum) % parseFloat(secondNum) * 100) / 100;
+}
+
+const exponential = function(firstNum, secondNum) {
+    return Math.round((parseFloat(firstNum) ** parseFloat(secondNum)) * 100) / 100;
 }
 
 // return function call based on operator used
@@ -28,67 +41,80 @@ const operate = function(operator, firstNum, secondNum) {
     switch(operator) {
         case '+':
             return add(firstNum, secondNum);
-        case '-':
+        case '−':
             return subtract(firstNum, secondNum);
-        case '*':
+        case '×':
             return multiply(firstNum, secondNum);
-        case '/':
+        case '÷':
             return divide(firstNum, secondNum);
+        case '%':
+            return modulus(firstNum, secondNum);
+        case '^':
+            return exponential(firstNum, secondNum);
         default:
             return 'Invalid Operator';
+    }
+}
+
+const checkFirstElement = function(calcValueElement) {
+    if (
+        (calcValueElement == '+') ||
+        (calcValueElement == '−') ||
+        (calcValueElement == '×') ||
+        (calcValueElement == '÷') ||
+        (calcValueElement == '%') ||
+        (calcValueElement == '^') ||
+        (calcValueElement == '=') ||
+        (calcValueElement == 0)
+    ) {
+        calcValueList = [];
+        return;
+    }
+}
+
+const calculate = function(button) {
+    displayResult = '';
+    calcValueList.push(button.textContent);
+    checkFirstElement(calcValueList[0]);
+    switch(button.textContent) {
+        case '+':
+            operator = '+';
+            break;
+        case '−':
+            operator = '−';
+            break;
+        case '×':
+            operator = '×';
+            break;
+        case '÷':
+            operator = '÷';
+            break;
+        case '%':
+            operator = '%';
+            break;
+        case '^':
+            operator = '^';
+            break;
+    }
+    displayResult = calcValueList.join('');
+    subdisplayText.textContent = displayResult;
+    if (calcValueList.includes('=')) {
+        displayResult = displayResult.slice(0, -1);
+        subdisplayResults.textContent = displayResult;
+        let nums = displayResult.split(operator);
+        firstNum = nums[0];
+        secondNum = nums[1];
+        let result = operate(operator, firstNum, secondNum);
+        subdisplayResults.textContent = result;
+        calcValueList = [];
+        calcValueList.push(result)
+    } else if (calcValueList.includes('AC')) {
+        calcValueList = [];
+        subdisplayText.textContent = 0;
+        subdisplayResults.textContent = 0;
     }
 }
 
 buttons.forEach(function(button) {
     button.addEventListener('click', () => calculate(button));
 });
-
-const calculate = function(button) {
-    displayResult = '';
-    calcValueList.push(button.textContent);
-    console.log(calcValueList);
-    if (
-        (calcValueList.includes('÷')) ||
-        (calcValueList.includes('×')) ||
-        (calcValueList.includes('+')) ||
-        (calcValueList.includes('-'))
-    ) {
-        operator = calcValueList[calcValueList.length - 1];
-        console.log(operator);
-    }
-    displayResult = calcValueList.join('');
-    displayScreen.textContent = displayResult;
-    if (calcValueList.includes('=')) {
-        displayResult = displayResult.slice(0, -1);
-        displayScreen.textContent = displayResult;
-        if (displayResult.includes('÷')) {
-            let nums = displayResult.split('÷');
-            firstNum = nums[0];
-            secondNum = nums[1];
-            console.log(divide(firstNum, secondNum));
-        } else if (displayResult.includes('×')) {
-            let nums = displayResult.split('×');
-            firstNum = nums[0];
-            secondNum = nums[1];
-            console.log(multiply(firstNum, secondNum));
-        } else if (displayResult.includes('+')) {
-            let nums = displayResult.split('+');
-            firstNum = nums[0];
-            secondNum = nums[1];
-            console.log(add(firstNum, secondNum));
-        } else if (displayResult.includes('−')) {
-            let nums = displayResult.split('−');
-            firstNum = nums[0];
-            secondNum = nums[1];
-            console.log(subtract(firstNum, secondNum));
-        } else {
-            console.log('invalid operator');
-        }
-    }
-}
-
-console.log(operate('+',2,2));
-console.log(operate('-',2,2));
-console.log(operate('*',2,2));
-console.log(operate('/',2,2));
-console.log(operate('/',2,0))
